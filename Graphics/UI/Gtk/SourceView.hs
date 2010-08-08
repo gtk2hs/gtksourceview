fe3a3795a1cf1e6a5fecae2d25555ca3072d5296
Andy Stewart <lazycat.manatee@gmail.com>
2010-08-08 05:48:16 +0000

    Add SourceGutter/SourceUndoManager in SourceView.chs.

--- Graphics/UI/Gtk/SourceView.hs
+++ Graphics/UI/Gtk/SourceView.hs
@@ -1,19 +1,25 @@
 module Graphics.UI.Gtk.SourceView (
 	module Graphics.UI.Gtk.SourceView.SourceBuffer,
+	module Graphics.UI.Gtk.SourceView.SourceGutter,
 	module Graphics.UI.Gtk.SourceView.SourceIter,
 	module Graphics.UI.Gtk.SourceView.SourceLanguage,
 	module Graphics.UI.Gtk.SourceView.SourceLanguageManager,
+	module Graphics.UI.Gtk.SourceView.SourceMark,
 	module Graphics.UI.Gtk.SourceView.SourceStyle,
 	module Graphics.UI.Gtk.SourceView.SourceStyleScheme,
 	module Graphics.UI.Gtk.SourceView.SourceStyleSchemeManager,
-	module Graphics.UI.Gtk.SourceView.SourceView
+	module Graphics.UI.Gtk.SourceView.SourceUndoManager,
+	module Graphics.UI.Gtk.SourceView.SourceView,
 	) where
 
 import Graphics.UI.Gtk.SourceView.SourceBuffer
+import Graphics.UI.Gtk.SourceView.SourceGutter
 import Graphics.UI.Gtk.SourceView.SourceIter
 import Graphics.UI.Gtk.SourceView.SourceLanguage
 import Graphics.UI.Gtk.SourceView.SourceLanguageManager
+import Graphics.UI.Gtk.SourceView.SourceMark
 import Graphics.UI.Gtk.SourceView.SourceStyle
 import Graphics.UI.Gtk.SourceView.SourceStyleScheme
 import Graphics.UI.Gtk.SourceView.SourceStyleSchemeManager
+import Graphics.UI.Gtk.SourceView.SourceUndoManager
 import Graphics.UI.Gtk.SourceView.SourceView
