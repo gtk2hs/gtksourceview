40d5414a27af02ea85c4cde338ed66fb12c3719f
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2013-12-01 20:45:00 +1300

    Build both GTK+2 and GTK+3 packages

--- install-both.sh
+++ install-both.sh
@@ -3,10 +3,10 @@
 cabal clean
 mv gtksourceview2.cabal-renamed gtksourceview2.cabal || true
 mv gtksourceview3.cabal gtksourceview3.cabal-renamed || true
-cabal-src-install "$@"
+cabal install "$@"
 
 cabal clean
 mv gtksourceview3.cabal-renamed gtksourceview3.cabal || true
 mv gtksourceview2.cabal gtksourceview2.cabal-renamed || true
-cabal-src-install "$@"
+cabal install "$@"
 
